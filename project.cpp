7e5f3c5b3aac21a4977f178daf993cf2d7d99a3f
raalicee <ralycia1808@gmail.com>
2025-05-17 05:24:21 +0700

    update menu

--- project.cpp
+++ project.cpp
@@ -4,79 +4,134 @@
 #include <cstring>
 using namespace std;
 
-struct buku{
+struct Buku{
     char judul[100];
     char penulis[100];
     char penerbit[100];
-    int* tahun;
-    int* stock;
-    buku *prev;
-    buku *next;
+    int tahun;
+    int stock;
+    Buku *prev;
+    Buku *next;
 };
 
-buku *kepala = NULL;
-buku *ekor = NULL;
+Buku *kepala = NULL;
+Buku *ekor = NULL;
 
-void bubbleSort(buku arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j].judul, arr[j + 1].judul) > 0) {
-                swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
+FILE *projek;
 
-// Fungsi untuk memuat data dari file ke array lagu
-void simpanData(buku arr[], int &jumlah) {
-    FILE *projek = fopen("projek.txt", "r");
+void bacadata() {
+    projek = fopen("projek.txt", "r");
     if (projek == NULL) {
         cout << "File tidak ditemukan atau tidak bisa dibuka!" << endl;
         return;
     }
 
-    jumlah = 0; // reset jumlah sebelum memuat data
-    while (fscanf(projek, "%[^;];%[^;];%[^;];%d;%d\n", 
-                 arr[jumlah].judul, arr[jumlah].penulis, 
-                 arr[jumlah].penerbit, arr[jumlah].tahun, 
-                 arr[jumlah].stock) != EOF) {
-        jumlah++;
+    // Kosongkan list
+    kepala = ekor = NULL;
+    while (!feof(projek)) {
+    Buku *baru = new Buku;
+        baru->next = NULL;
+        baru->prev = NULL;
+
+    if(fscanf(projek, "%[^;];%[^;];%[^;];%d;%d\n", 
+                 baru->judul, baru->penulis, baru->penerbit,
+                 &baru->tahun, &baru->stock) != EOF) {
+      
+    }
+    // Tambah ke double linked list
+        if (kepala == NULL) {
+            kepala = ekor = baru;
+        } else {
+            ekor->next = baru;
+            baru->prev = ekor;
+            ekor = baru;
+        }
     }
     fclose(projek);
 }
 
 void tambahbuku(){
-    string judul, penulis, penerbit;
-    int tahun, stock;
-    // buku *baru = new buku;
-    cout << "Judul Buku: ";
-    getline(cin, judul);
-    cin.ignore();
-    cout << "Penulis Buku: ";
-    getline(cin, penulis);
-    cin.ignore();
-    cout << "Penerbit Buku: ";
-    getline(cin, penerbit);
-    cin.ignore();
-    cout << "Tahun Terbit: ";
-    cin >> tahun;
-    cin.ignore();
-    cout << "Stock Buku: ";
-    cin >> stock;
-    
+    int jumlahbaru;
+    projek = fopen("projek.txt", "a");
+            if (projek == NULL) {
+                cout << "Error membuka file!" << endl;
+                exit(1);
+            }
+            cout << "Jumlah buku yang diinput: ";
+            cin >> jumlahbaru;
+            cin.ignore();
+
+            for (int i = 0; i < jumlahbaru; i++) {
+                cout << endl;
+                Buku *baru = new Buku;
+                baru->prev = NULL;
+                baru->next = NULL;
+
+                cout << "Judul buku: ";
+                cin.getline(baru->judul, 100);
+                cout << "Penulis: ";
+                cin.getline(baru->penulis, 100);
+                cout << "Penerbit: ";
+                cin.getline(baru->penerbit, 100);
+                cout << "Tahun terbit: ";
+                cin >> baru->tahun;
+                cin.ignore();
+                cout << "Stock: ";
+                cin >> baru->stock;
+                cin.ignore();
+
+                fprintf(projek, "%s;%s;%s;%d;%d\n",
+                baru->judul, baru->penulis, baru->penerbit,
+                baru->tahun, baru->stock);
+
+                if (kepala == NULL) {
+                kepala = ekor = baru;
+                } else {
+                ekor->next = baru;
+                baru->prev = ekor;
+                ekor = baru;}
+            }
+            
+            cout << "Data berhasil ditambahkan!" << endl;
+            fclose(projek);
+}
+void tampilkanBuku() {
+    if (kepala == NULL) {
+        cout << "Tidak ada data buku!" << endl;
+        return;
+    }
+
+    Buku *temp = kepala;
+    int no = 1;
+    while (temp != NULL) {
+        cout << "\nBuku ke-" << no++ << endl;
+        cout << "Judul     : " << temp->judul << endl;
+        cout << "Penulis   : " << temp->penulis << endl;
+        cout << "Penerbit  : " << temp->penerbit << endl;
+        cout << "Tahun     : " << temp->tahun << endl;
+        cout << "Stock     : " << temp->stock << endl;
+        temp = temp->next;
+    }
 }
 
 int main() {
     int menu;
+    char ulang;
+    do{
     cout<<"1. Input data buku"<<endl;
     cout<<"2. Tampilkan data buku"<<endl;
     cout<<"3. Cari buku"<<endl;
-    cout<<"pilih menu; "; cin>>menu;
+    cout<<"pilih menu: "; cin>>menu;
     system("cls");
 
     if(menu == 1){
         tambahbuku();
+    } else if (menu == 2){
+        bacadata();
+        tampilkanBuku();
     }
-    
-
+    cout << "\nApakah Anda ingin melakukan transaksi lain? (y/n): ";
+    cin >> ulang;
+    system("cls");
+    } while (ulang == 'y' || ulang == 'Y');
 }
